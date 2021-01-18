4e6cb7c89412e766df247307e3e79bc5e6ace4a9
pedroblanch <pedroblanch@iesjulioverne.es>
2021-01-18 13:19:54 +0100

    commit

---
+++ varios/datosPersona1.php
@@ -0,0 +1,20 @@
+<body>
+<form action="datosPersona2.php" method="get">
+<br>
+Apellidos: <input type="text" name="apellidos">
+<br>
+Nombre: <input type="text" name="nombre">
+<br>
+Provincia: <input type="text" name="provincia">
+<br>
+Familia numerosa: <input type="checkbox" name="familiaNumerosa" value=true>
+<br>
+Soltero:<input type="radio" name="estadoCivil" value="soltero">
+Casado:<input type="radio" name="estadoCivil" value="casado">
+Separado:<input type="radio" name="estadoCivil" value="separado">
+Viudo:<input type="radio" name="estadoCivil" value="viudo">
+<br>
+<input type="submit" value="Aceptar">
+</form>
+
+</body>
---
+++ varios/datosPersona2.php
@@ -0,0 +1,46 @@
+<?php
+//recojo los parámetros de entrada
+$apellidos=$_GET['apellidos'];
+$nombre=$_GET['nombre'];
+$estadoCivil=$_GET['estadoCivil'];
+$provincia=$_GET['provincia'];
+$familiaNumerosa=$_GET['familiaNumerosa'];
+
+if(empty($apellidos)){
+    echo("faltan los apellidos");
+    exit;
+}
+if(empty($nombre)){
+    echo("falta el nombre");
+    exit;
+}
+if(empty($provincia)){
+    echo("falta la provincia");
+    exit;
+}
+
+if(!isset($familiaNumerosa)){
+    $repite=false;
+}
+else{
+    $repite=true;
+}
+
+if(!isset($estadoCivil)){
+    echo ("Falta el estado civil");
+    exit;
+}
+
+//si llega aquí los parámetros son correctos
+echo("Nombre: $nombre<br>");
+echo("Apelidos: $apellidos<br>");
+echo("Provincia: $provincia<br>");
+echo("Estado civil: $estadoCivil<br>");
+echo("Familia numerosa:");
+if($familiaNumerosa){
+    echo " si";
+}
+else{
+    echo " no";        
+}
+?>
